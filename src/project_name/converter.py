e4acb1d1c31e748bbc5a98554dfd4163413d8dad
lucius.mortified <leo.fed97@gmail.com>
2019-11-06 18:21:00 +1000

    #4253 done

--- src/project_name/converter.py
+++ src/project_name/converter.py
@@ -9,10 +9,16 @@ bp = Blueprint('converter', __name__, url_prefix='/')
 @bp.route('/convert', methods=['POST'])
 def convert():
     result = ""
-    json_data = request.get_json()
-    if isinstance(json_data, list):
-        for p in json_data:
+    data = request.get_json()
+    if isinstance(data, list):
+        result += "<ul>"
+        for p in data:
+            result += "<li>"
             for key in p.keys():
                 result += "<"+key+">"+p.get(key, '')+"</"+key+">"
-            result += "\n"
+            result += "</li>\n"
+        result += "</ul>"
+    else:
+        for key in data:
+            result += "<" + key + ">" + data.get(key, '') + "</" + key + ">"
     return result
